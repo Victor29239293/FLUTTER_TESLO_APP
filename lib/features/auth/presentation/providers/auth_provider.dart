cf92dfd474d179e2a0431d2c1e99fa2aa357a0af
Victor29239293 <vcelir2@unemi.edu.ec>
2025-08-31 23:02:44 -0500

    auth_provider

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,12 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_teslo_app/features/auth/infrastructure/infrastructure.dart';
 import 'package:flutter_teslo_app/features/auth/infrastructure/repository/auth_repository_impl.dart';
 
 import '../../domain/domain.dart';
 
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
-  final authRepository = AuthRepositoryImpl(
-    
-  );
+  final authRepository = AuthRepositoryImpl();
   return AuthNotifier(authRepository: authRepository);
 });
 
@@ -15,17 +14,35 @@ class AuthNotifier extends StateNotifier<AuthState> {
 
   AuthNotifier({required this.authRepository}) : super(AuthState());
 
-  void loginUser(String email , String password) async{
-    
+  void loginUser(String email, String password) async {
+    await Future.delayed(const Duration(seconds: 2));
 
+    try {
+      final user = await authRepository.login(email, password);
+      _setLogoutUser(user);
+    } on WrongCredentials {
+      logoutUser('Credenciales no son correcta');
+    } catch (e) {
+      logoutUser('Error inesperado');
+    }
   }
 
-  void registerUser(String email , String password)async{
+  void registerUser(String email, String password) async {}
 
+  void checkAuthStatus() async {}
+
+  void _setLogoutUser(User user) async {
+    // TODO: 
+    state = state.copyWith(authStatus: AuthStatus.authenticated, user: user);
   }
 
-  void checkAuthStatus()async{
-
+  Future<void> logoutUser(String? errorMessage) async {
+    // TODO: LIMPIAR TOKEN
+    state = state.copyWith(
+      authStatus: AuthStatus.notAuthenticated,
+      user: null,
+      errorMessage: errorMessage,
+    );
   }
 }
 
